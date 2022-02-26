815ef9779878e2af942c59b21c72eac6028acd14
skaothic <37940657+skaothic@users.noreply.github.com>
2022-02-26 11:34:31 +0100

    commit git hub pages ok

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -7,15 +7,19 @@ import { CityService } from '../service/city.service';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
+  //récuperation du tableau cré sur l'onglet home pour la mise en place des infos dans la page détails
+
   public depart:string=this.cityService.travel[0].dep
   public arrivee:string=this.cityService.travel[0].arr
   public date:string=this.cityService.travel[0].date
   public hour:string=this.cityService.travel[0].hour
   public mapURL:string=""
+
   constructor(private cityService:CityService) { }
 
   ngOnInit(): void {
-    this.mapURL="http://maps.google.com/maps?saddr="+this.cityService.travel[0].depVal+"&daddr="+this.cityService.travel[0].arrVal+"&ie=UTF8&output=embed"
+    //création du lien de la carte Google intégré avec utilisation des "unique_name" pour les pins départ et arrivée avec différent type de voyage(avion,voiture et train)
+    this.mapURL="https://maps.google.com/maps?saddr="+this.cityService.travel[0].depVal+"&daddr="+this.cityService.travel[0].arrVal+"&ie=UTF8&output=embed"
    
   }
 
